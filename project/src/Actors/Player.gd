937a73119602110992aae878f09154d738ccb029
David Rodriguez <dave00dark@gmail.com>
2021-01-29 01:19:20 -0400

    Dash del Player

--- project/src/Actors/Player.gd
+++ project/src/Actors/Player.gd
@@ -1,9 +1,13 @@
 extends KinematicBody2D
 
+const LAND_TILE = 3
+
 export (int) var DEFAULT_SPEED = 250
 export (int) var DASH_SPEED = 1000
 var speed: int = DEFAULT_SPEED
 var velocity = Vector2()
+
+onready var tilemap_floor = get_parent().get_tilemap_floor()
 
 func get_input():
 	velocity = Vector2()
@@ -23,6 +27,7 @@ func get_input():
 func _physics_process(delta):
 	get_input()
 	velocity = move_and_slide(velocity)
+
 
 func animate():
 	if velocity.x == 0 and velocity.y == 0:
@@ -52,8 +57,18 @@ func animate():
 
 # Dash movement
 func dash():
+	# Verificar que puede llegar al otro lado
+	var position_in_front = position + velocity.normalized() * 200
+	var a = tilemap_floor.world_to_map(tilemap_floor.global_transform.xform_inv(position_in_front))
+	if tilemap_floor.get_cellv(a) == LAND_TILE:
+		# Si es tierra, se quita la collision mask 2 que es la de piso
+		set_collision_mask_bit(1,false)
 	speed = DASH_SPEED
 	$DashTimer.start()
+		
 
 func _on_DashTimer_timeout():
+	# Restauramos las configuraciones
 	speed = DEFAULT_SPEED
+	set_collision_mask_bit(1,true)
+	
