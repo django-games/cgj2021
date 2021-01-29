4d3b8f754a3a336b3cd4d67075601f4d92e850bc
Gustavo Castellanos <gustavoaca1997@gmail.com>
2021-01-28 23:04:20 -0400

    Bigger islands

--- project/src/Levels/LevelTemplate.gd
+++ project/src/Levels/LevelTemplate.gd
@@ -1,13 +1,15 @@
 extends Node2D
 
-export var steps: = 1000
+const DIRECTIONS = [Vector2.ZERO,Vector2.RIGHT, Vector2.UP, Vector2.LEFT, Vector2.DOWN]
+
+export var steps: = 100000
 onready var tileMap = $Water
 onready var player = $Player
 var borders = Rect2()
 
 func _ready():
 	randomize()
-	borders = Rect2(Vector2(0, 0), Vector2(135, 20))
+	borders = Rect2(Vector2(-6400, -4128), Vector2(14832, 6820))
 	generate_level()
 	
 func generate_level():
@@ -16,4 +18,5 @@ func generate_level():
 	walker.queue_free()
 	player.set_position(map[0])
 	for location in map:
-		tileMap.set_cellv(location, 3)
+		for direction in DIRECTIONS:
+			tileMap.set_cellv(location+direction, 3)
