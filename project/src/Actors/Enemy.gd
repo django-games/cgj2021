38f0b1f631eda12ae4cfcb2ef89c06fc5826cb5d
Gustavo Castellanos <gustavoaca1997@gmail.com>
2021-01-30 17:18:14 -0400

    Fix animation of enemy

--- project/src/Actors/Enemy.gd
+++ project/src/Actors/Enemy.gd
@@ -1,7 +1,7 @@
 extends Area2D
 
 onready var player = get_parent().get_player()
-var speed := 200
+var speed := 190
 var velocity := Vector2()
 
 func _physics_process(delta):
@@ -9,9 +9,9 @@ func _physics_process(delta):
 	var player_position: Vector2 = player.position
 	
 	if position.distance_to(player_position) <= 200:
-		speed = 220
+		speed = 210
 	else:
-		speed = 200
+		speed = 190
 		
 	velocity = position.direction_to(player_position) * speed
 	animate()
@@ -30,19 +30,18 @@ func animate():
 	var x_move = ""
 	var y_move = ""
 	
-	if velocity.x > 0:
+	if velocity.x > 0 and velocity.x > abs(velocity.y):
 		x_move = '_right'
-	elif velocity.x < 0:
+	elif velocity.x < 0 and abs(velocity.x) > abs(velocity.y):
 		x_move = '_left'
 		
-	if velocity.y > 0:
+	if velocity.y > 0 and velocity.y > abs(velocity.x):
 		y_move = '_back'
-	elif velocity.y < 0:
+	elif velocity.y < 0 and abs(velocity.y) > abs(velocity.x):
 		y_move = '_front'
 		
 	var move = 'move' + x_move + y_move
 	$AnimatedSprite.play(move)
 
-
-func _on_PlayerDetector_body_entered(body):
-	body.die()
+func _on_Enemy_body_entered(body):
+	body.die() # Replace with function body.
