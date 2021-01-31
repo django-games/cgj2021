339ee085c142d99c6fedb86d6222218c3d8a4123
Andrés Ignacio Torres <andresitorresm@gmail.com>
2021-01-31 10:58:59 -0400

    add missing script

---
+++ project/src/Misc/SFX.gd
@@ -0,0 +1,14 @@
+extends Control
+
+
+func SFX1_timeout():
+	$SFX1/AudioStreamPlayer2D.play()
+
+func SFX2_timeout():
+	$SFX2/AudioStreamPlayer2D.play()
+
+func SFX3_timeout():
+	$SFX3/AudioStreamPlayer2D.play()
+
+func SFX4_timeout():
+	$SFX4/AudioStreamPlayer2D.play()
